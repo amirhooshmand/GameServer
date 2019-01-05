ec8caf4671aa6b10003600e4faf4d0bd7bd71098
Amir Hossein <amir.hsmart@gmail,com>
2019-01-06 00:33:58 +0330

    tt

--- Bindings/Constants.cs
+++ Bindings/Constants.cs
@@ -6,6 +6,7 @@ namespace Bindings
 {
     class Constants
     {
-        public const int MAX_PLAYER = 100;
+        public const int MAX_PLAYER = 10;
+        public string str = "test";
     }
 }
